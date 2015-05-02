582aeca6fe61b9e1758af290d49815e1dfa027a4
Vivek Seth <Vivekseth.m@gmail.com>
2015-05-02 16:32:07 -0400

    using dirty hack to use variables

--- nginx-code-site.sh
+++ nginx-code-site.sh
@@ -1,6 +1,6 @@
 
 set -e;
-erb url="$1" port="$2" ./code-site.erb > /etc/nginx/sites-available/$1;
+(echo "<% url='$1'; port='$2' %>" && cat code-site.erb && echo "") | erb > /etc/nginx/sites-available/$1;
 ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1;
 nginx -s reload;
 echo "successfully created site `$1`"
