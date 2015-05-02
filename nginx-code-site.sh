224c8360bb2c33ea1a17e5299c446e22129980dd
root <root@vivek.server>
2015-05-02 16:57:02 -0400

    working implementation

--- nginx-code-site.sh
+++ nginx-code-site.sh
@@ -1,6 +1,6 @@
 
 set -e;
-(echo "<% url='$1'; port='$2' %>" && cat code-site.erb && echo "") | erb > /etc/nginx/sites-available/$1;
+(echo "<% url='$1'; port='$2' %>" && cat /www/nginx-subdomain-creation/code-site.erb && echo "") | erb > /etc/nginx/sites-available/$1;
 ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1;
 nginx -s reload;
-echo "successfully created site `$1`"
+echo "successfully created site $1"
--- nginx-static-site.sh
+++ nginx-static-site.sh
@@ -1,6 +1,6 @@
 
 set -e;
-(echo "<% url='$1'; path='$2' %>" && cat static-site.erb && echo "") | erb > /etc/nginx/sites-available/$1;
+(echo "<% url='$1'; path='$2' %>" && cat /www/nginx-subdomain-creation/static-site.erb && echo "") | erb > /etc/nginx/sites-available/$1;
 ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1;
 nginx -s reload;
-echo "successfully created site `$1`"
+echo "successfully created site $1"
